711c550234827e2c62f49c0a245e272b79753853
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-08 05:20:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,5 +11,11 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  
+  birthday_kids.each do |name, age|
+      if (age < 10)
+        puts "Happy Birthday #{name}! You are now #{age} years old!"
+      else
+        puts "You are too old for this."
+      end
+  end
 end
